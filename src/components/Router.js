e569c427b2f96e3bfd256864e86622126ada3ae2
CTD <qpflapffhs76@gmail.com>
2022-01-06 20:56:26 +0900

    Firebase Auth

---
+++ src/components/Router.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+const AppRouter = ({ isLoggedIn }) => {
+  return (
+    <Router>
+      {isLoggedIn && <Navigation />}
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Redirect from="*" to="/" />
+          </>
+        ) : (
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+            <Redirect from="*" to="/" />
+          </>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
