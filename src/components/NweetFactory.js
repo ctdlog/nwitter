71a6b570dda16959fa6225658b09a2fd92853c64
CTD <qpflapffhs76@gmail.com>
2022-01-20 15:35:42 +0900

    Styles + Github 배포

---
+++ src/components/NweetFactory.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import {
+  addDoc,
+  collection,
+  //getDocs,
+  //where,
+} from "firebase/firestore";
+import { ref, uploadString, getDownloadURL } from "@firebase/storage";
+import { v4 as uuidv4 } from "uuid";
+import { dbService, storageService } from "fBase";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+const NweetFactory = ({ userObj }) => {
+  const [nweet, setNweet] = useState("");
+  const [attachment, setAttachment] = useState("");
+
+  const onSubmit = async (event) => {
+    if (nweet === "") {
+      return;
+    }
+    event.preventDefault();
+    let attachmentUrl = "";
+    if (attachment !== "") {
+      //파일 경로 참조 만들기
+      const fileRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+      //storage 참조 경로로 파일 업로드 하기
+      const uploadFile = await uploadString(fileRef, attachment, "data_url");
+      console.log(uploadFile);
+      //storage에 있는 파일 URL로 다운로드 받기
+      attachmentUrl = await getDownloadURL(uploadFile.ref);
+    }
+    //트윗할때, 메시지와 사진도 같이 firestore에 생성
+    const nweetPosting = {
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+      attachmentUrl,
+    };
+    await addDoc(collection(dbService, "nweets"), nweetPosting);
+    setNweet("");
+    setAttachment("");
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+
+  const onClearAttachment = () => setAttachment("");
+
+  return (
+    <form onSubmit={onSubmit} className="factoryForm">
+      <div className="factoryInput__container">
+        <input
+          className="factoryInput__input"
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="&rarr;" className="factoryInput__arrow" />
+      </div>
+      <label htmlFor="attach-file" className="factoryInput__label">
+        <span>Add photos</span>
+        <FontAwesomeIcon icon={faPlus} />
+      </label>
+      <input
+        id="attach-file"
+        type="file"
+        accept="image/*"
+        onChange={onFileChange}
+        style={{
+          opacity: 0,
+        }}
+      />
+      {attachment && (
+        <div className="factoryForm__attachment">
+          <img
+            alt=""
+            src={attachment}
+            style={{
+              backgroundImage: attachment,
+            }}
+          />
+          <div className="factoryForm__clear" onClick={onClearAttachment}>
+            <span>Remove</span>
+            <FontAwesomeIcon icon={faTimes} />
+          </div>
+        </div>
+      )}
+    </form>
+  );
+};
+export default NweetFactory;
--- src/components/Router.js
+++ src/components/Router.js
@@ -11,21 +11,30 @@ import Profile from "routes/Profile";
 import Navigation from "components/Navigation";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-const AppRouter = ({ isLoggedIn, userObj }) => {
+const AppRouter = ({ refreshUser, isLoggedIn, userObj }) => {
   return (
     <Router>
-      {isLoggedIn && <Navigation />}
+      {isLoggedIn && <Navigation userObj={userObj} />}
       <Switch>
         {isLoggedIn ? (
-          <>
+          <div
+            style={{
+              maxWidth: 890,
+              width: "100%",
+              margin: "0 auto",
+              marginTop: 80,
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
             <Route exact path="/">
               <Home userObj={userObj} />
             </Route>
             <Route exact path="/profile">
-              <Profile />
+              <Profile refreshUser={refreshUser} userObj={userObj} />
             </Route>
             <Redirect from="*" to="/" />
-          </>
+          </div>
         ) : (
           <>
             <Route exact path="/">
